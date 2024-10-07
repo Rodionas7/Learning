9fc711c9e2b4fb11edba71087e3cf2175ad57559
Rodionas <rodionas.papadopoulos@ghd.com>
2024-10-07 16:16:40 +0100

    Strict Mode

--- learning.jsx
+++ learning.jsx
@@ -1,19 +1,19 @@
 // JSX (JavaScript XML): A syntax extension of JavaScript that looks like HTML, but allows you to write elements in a more declarative way inside JavaScript
 // Functional Component: This is a JavaScript function that returns JS. This should be a single element, so we usually return a 'div'.
 // Props (Properties): Allow you to pass data from a parent component to a child component
-// State: Used to store dynamic data in a component
-// Hook: A special function that allows you to manage state
-    // useState: 
+// State: Data that needs to be tracked in an application
+// Hook: A special function that allows to use different React features inside components. They must be called inside function components, at top level and cannot be conditional. 
+    // useState: Allows to track state in a function component
     // useEffect:
 
 import React from "react";
-    
-
-const [count, setCount] = useState(0);
+import { useState } from "react";
 
 
 // Creating component using function notation
 function MyComponentRegular(props) {  // Props
+    const [count, setCount] = useState(0);
+
     return (
         <h1>My name is {props.name} and I am {props.age} years old!</h1>
     )
