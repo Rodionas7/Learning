3d1ed1d81970dda39245c33d9728b123ccc1f99f
Rodionas7 <54659934+Rodionas7@users.noreply.github.com>
2024-11-18 23:16:06 +0000

    http request setup

--- Backend/app/routes/__init__.py
+++ Backend/app/routes/__init__.py
@@ -5,6 +5,6 @@ from Backend.app.routes.user import router as user_router
 router = APIRouter() 
 router.include_router(
     router=user_router,  # The router object to be included in the application
-    prefix="/users", # The prefix or base path that will be added to all the routes. For example, if the route is defined as @router.get("/user") then the endpoint will be accessible at "api/users/user".
+    prefix="/users",  # The prefix or base path that will be added to all the routes. For example, if the route is defined as @router.get("/user") then the endpoint will be accessible at "api/users/user".
     tags=["users"]  # Tags help to group and organise routes (i.e. contain related endpoints)
 )
